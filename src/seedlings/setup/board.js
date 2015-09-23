7b1d19d1ccf03c2dec0ec479b36a5cffa4db6cab
Andrew Kwon <askwon@ucdavis.edu>
2015-09-23 12:31:30 -0700

    Updated ready sounds for chime5

--- src/seedlings/setup/board.js
+++ src/seedlings/setup/board.js
@@ -97,7 +97,7 @@ function setup(socket, seedlingNum, callback) {
         var topical = ['cash','police','vroom','water1','water2','horhorhor'];
         var dumb = ['baa','horse','quak1','quak2','meow1','meow2','moo1','moo2','moo3','moo4','moo5'];
         var no = ['error1','error2','error3','no1','no2','no3','no4'];
-        var ready = ['chime1','chime2','chime3','chime4','ready1','ready2','ready3','ready4'];
+        var ready = ['chime1','chime2','chime3','chime4','chime5','ready1','ready2','ready3','ready4'];
         var celebratory = ['sad','roar'];
         soundObj = new SoundsObj(topical, dumb, no, ready, celebratory);
 
