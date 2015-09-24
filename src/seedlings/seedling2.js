e99a6e813a1ed0e86febafa93f58468a502fe19f
Andrew Kwon <askwon@ucdavis.edu>
2015-09-23 17:53:49 -0700

    Completed transition from relative to absolute paths on all seedlings

--- src/seedlings/seedling2.js
+++ src/seedlings/seedling2.js
@@ -9,13 +9,11 @@ var IP = 'http://169.237.123.19:600' + seedlingNum;
 var socket = require('socket.io-client')(IP);
 var path = require('path');
 var obj = null;
-console.log("required: "+path.join(__dirname, '..', 'jsLibrary', 'led.js'));
 var led = require(path.join(__dirname, '..', 'jsLibrary', 'led.js'));
 
 socket.emit('checkin', 'SEEDLING' + (seedlingNum+1));
 
 // Init the board as well as the led socket listeners
-console.log("required: " + path.join(__dirname, 'setup', 'board.js'));
 var initBoard = require(path.join(__dirname, 'setup', 'board.js'));
 initBoard.setup(socket, seedlingNum, function(data){
 	obj = data;
