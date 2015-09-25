1e6e62d37bda278e5840b5b324dcbde70ecef484
Andrew Kwon <askwon@ucdavis.edu>
2015-09-24 18:57:01 -0700

    Moved old code into folder

--- src/jsLibrary/websockets.js
+++ src/jsLibrary/websockets.js
@@ -114,7 +114,6 @@ function countdown() {
 // Make sure to broadcast to all when the button is pressed
 countdown();
 
-
 ////////////////////////////////////////////////
 //  web
 ////////////////////////////////////////////////
@@ -156,12 +155,7 @@ io.on('connection', function (socket) {
         percentages: heightCalcGeneric(story[lastActiveSeedling].parts[currentPart]) });
   });
 
-  socket.on('ping', function() {
-      console.log("ping");
-      io.sockets.emit('pong');
-  });
-
-  // // Front-end simulation of a button press
+  // Front-end simulation of a button press
   socket.on('sim button', function(seedlingNum) {
       if(!seedlings[seedlingNum].buttonPressed){
           seedlings[seedlingNum].buttonPressed = true;
@@ -172,124 +166,8 @@ io.on('connection', function (socket) {
   // Update the seconds in the web page
   setInterval(function(){
     socket.emit('updateTime', seconds);
-  },1000);
+  }, 1000);
 
-  socket.on('checkin', function(data){
-    console.log('###  webClient checkin')
-    console.log(data)
-  })
-
-  socket.on('webMoveMotor', function(data){
-    console.log('motor:' + data.motor + '  steps:' + data.steps + '  direction:' + data.dir)
-    if(beagleOnline){
-      console.log('beagle ONLINE')
-      beagle.emit('webMoveMotor', data);
-    }
-  })
-
-  socket.on('testCycle', function(){
-    console.log('*** testCycle');
-    //seseme.testCycle(1);
-  })
-
-  socket.on('updateFrequency', function(data){
-    console.log('update acceleration: ' + data)
-    if(beagleOnline)  beagle.emit('updateFrequency', data);
-  })
-
-  socket.on('updateRPM', function(data){
-    console.log('update rpm: ' + data)
-    if(beagleOnline)  beagle.emit('updateRPM', data);
-  })
-
-  socket.on('resetPosition', function(motorName){
-    console.log('reset position for: ' + motorName);
-    if(beagleOnline)  beagle.emit('resetPosition', motorName);
-  })
-
-  socket.on('loopPillars', function(){
-    console.log('LOOPING seseme');
-    if(beagleOnline)  beagle.emit('loopPillars');
-  })
-
-  socket.on('setHSL', function(data){
-    console.log(data)
-    hue.setHSL(data)
-  })
-
-  socket.on('ledColor', function(data){
-    console.log(data)
-    if(seedlings[0].online)  seedlings[0].socket.emit('ledColor', data);
-  })
-
-  socket.on('ledBrightness', function(data){
-    console.log(data)
-    if(seedlings[0].online)  seedlings[0].socket.emit('ledBrightness', data);
-  })
-
-  socket.on('ledPercentage', function(data){
-    console.log(data)
-    if(seedlings[0].online)  seedlings[0].socket.emit('ledPercentage', data);
-  })
-
-  socket.on('lightsOn', function(data){
-    console.log("lights on")
-    hue.turnOn()
-    if(seedlings[0].online) seedlings[0].socket.emit('lightsOn');
-  })
-
-  socket.on('lightsOff', function(data){
-    console.log("lights off")
-    hue.turnOff()
-    if(seedlings[0].online) seedlings[0].socket.emit('lightsOff', data);
-  })
-
-  socket.on('partyOn', function(){
-    hue.partyOn()
-  })
-
-  socket.on('partyOff', function(){
-    hue.partyOff()
-  })
-
-  socket.on('lightTrail', function(data){
-    if(seedlings[0].online) seedlings[0].socket.emit('lightTrail', data);
-  })
-
-  socket.on('fadeCircle', function(data){
-    if(seedlings[0].online) seedlings[0].socket.emit('fadeCircle', data);
-  })
-
-  socket.on('blink', function(data){
-    if(seedlings[0].online) seedlings[0].socket.emit('blink', data);
-  })
-
-  socket.on('moveMotorJack', function(data){
-    var motorName = data.name
-    var newPosition = parseInt(data.position)
-    console.log(motorName + '  position:' + newPosition)
-    if(beagleOnline)  beagle.emit('moveMotorJack', data);
-  })
-
-  socket.on('whereWeAt', function(){
-
-  })
-
-  socket.on('moveInUnison', function(data){
-    if(beagleOnline)  beagle.emit('moveInUnison', data);
-  })
-
-  socket.on('moveInSimpleSequence', function(data){
-    if(beagleOnline)  beagle.emit('moveInSimpleSequence', data);
-  })
-
-  socket.on('nameOn', function(data){
-    if(seedlings[0].online)  seedlings[0].socket.emit('nameOn', data);
-  })
-
-  socket.on('nameOff', function(data){
-    if(seedlings[0].online)  seedlings[0].socket.emit('nameOff', data);
-  })
 });
 
 
@@ -353,7 +231,6 @@ function fadeCircleObj(targetColor, duration, diodePct){
     this.duration = duration;
     this.diodePct = diodePct;
 }
-
 
 function seedlingConnected(seedSocket, seedlingNum){
   var seedling = seedlings[seedlingNum];
