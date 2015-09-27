af2cc7bf4224f3283df35667c0375ce387e3ba03
Andrew Kwon <askwon@ucdavis.edu>
2015-09-27 16:00:13 -0700

    Switched the exported promise to a standard function to turn off the bulb

--- src/jsLibrary/lifx.js
+++ src/jsLibrary/lifx.js
@@ -12,29 +12,29 @@ var options = {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// [ NOTES ON LIFX BEHAVIOR ] 
+// [ NOTES ON LIFX BEHAVIOR ]
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // [ New story ]
-//		--> Use hardcoded hex value to change color 
-//		
-// [ Valid Button Press ] 
-//		--> Change to current part, will have hardcoded lifx value 
+//		--> Use hardcoded hex value to change color
+//
+// [ Valid Button Press ]
+//		--> Change to current part, will have hardcoded lifx value
 //			When you change colors, start at 0.75 and ramp down to 0.6 (12 seconds)
-//			
+//
 // [ Invalid Button Press ]
-//		--> Flash (1 down 2 down) 
-//		
+//		--> Flash (1 down 2 down)
+//
 // [ Idle Tier 1 Breathe ]
-//		--> random duration (go to 0.075 brightness and back to 0.5) 
+//		--> random duration (go to 0.075 brightness and back to 0.5)
 //
 // [ Idle Tier 2 Breathe ]
-//		--> after 4 minutes of breathing, turn it off 
+//		--> after 4 minutes of breathing, turn it off
 //
 // [ Login ]
-//		--> During	idle 1: (ignore), 
-//					idle 2: (Blink from off to 0.35, chill for 3 seconds, turn off)  
+//		--> During	idle 1: (ignore),
+//					idle 2: (Blink from off to 0.35, chill for 3 seconds, turn off)
 //							^lowest priority (don't let it interrupt)
-//					active: Same thing as new part 
+//					active: Same thing as new part
 //							(go to bright 0.75, ramp down to 0.6 for 3 seconds)
 //
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -44,7 +44,7 @@ function validButtonPress(color, factor) {
 	if(factor > 1) {
 		console.log("Error: Brightness factor greater than 1");
 		return;
-	} 
+	}
 
 	// Hardcoded properties for a behavior specific to a valid button press
 	var properties = {
@@ -54,21 +54,21 @@ function validButtonPress(color, factor) {
 		'duration'	: 2
 	};
 
-	// Promise to update the light, then ramp down 
+	// Promise to update the light, then ramp down
 	updateLight(properties);
-	if(!factor) 
+	if(!factor)
 		setTimeout(function() {rampDown(factor, 1.25)}, 12000);
 }
 
 // Function to update the light with custom properties that could take:
-// [ power / color / brightness / duration ] 
+// [ power / color / brightness / duration ]
 function updateLight(properties) {
 	console.log("Updating light color now");
 
 	// Configurations and custom headers to send to the API
 	options.body = JSON.stringify(properties);
 
-	// PUT http request to update the hue color 
+	// PUT http request to update the hue color
 	request(options, function(error, response, body) {
 		if(error) console.log("Error: " + error);
 	}); // end of request
@@ -81,26 +81,26 @@ function rampDown(factor, duration) {
 	// Configurations and custom headers to send to the API
 	options.body = JSON.stringify({
 		'brightness': 0.5 * factor,
-		'duration': duration 
+		'duration': duration
 	});
 
-	// PUT http request to ramp down 
+	// PUT http request to ramp down
 	request(options, function(error, response, body) {
-		if(error) console.log("Error: " + error); 
+		if(error) console.log("Error: " + error);
 	}); // end of request
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Idle State: Breathe 
+// Idle State: Breathe
 function breathe() {
-	// 2 seconds to fade on and fade off, 6 seconds of offtime 
+	// 2 seconds to fade on and fade off, 6 seconds of offtime
 	var offtime;
-		
+
 	// Main breathe logic using a recursive promise chain with a timeout of 6s
 	var breatheSequence = function() {
 		fadeOn(1)
 			.then( setTimeout(function() { return fadeOff(1); }, 1250))
-			.then( function() { 
+			.then( function() {
 				offtime = setTimeout(breatheSequence, 5000);
 			});
 	};
@@ -117,11 +117,11 @@ function fadeOn(duration) {
 			'brightness'	: 0.85,
 			'duration'		: duration,
 		});
-	
+
 		// PUT http request to fade on
 		request(options, function(error, response, body) {
 			if(error) reject(error);
-			else if(response.statusCode == 200 || response.statusCode == 201) 
+			else if(response.statusCode == 200 || response.statusCode == 201)
 				resolve();
 		}); // end of request
 	}); // end of promise
@@ -134,18 +134,33 @@ function fadeOff(duration) {
 			'power'		: 'off',
 			'duration'	: duration
 		});
-	
+
 		// PUT http request to fade off
 		request(options, function(error, response, body) {
 			if(error) reject(error);
-			else if(response.statusCode == 200 || response.statusCode == 201) 
+			else if(response.statusCode == 200 || response.statusCode == 201)
 				resolve();
 		}); // end of request
 	}); // end of promise
 }
 
+function turnOff(duration) {
+	// Configurations and custom headers to send to the API
+	options.body = JSON.stringify({
+		'power'		: 'off',
+		'duration'	: duration
+	});
+
+	// PUT http request to turn the bulb off
+	request(options, function(error, response, body) {
+		if(error) console.log(error);
+		else if(response.statusCode == 200 || response.statusCode == 201)
+			console.log("Turning off bulb now");
+	}); // end of request
+}
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// Idle State: Desperation 
+// Idle State: Desperation
 function desperation(states) {
 	options.uri = 'https://api.lifx.com/v1beta1/lights/'+id+'/cycle';
 	options.method = 'POST';
@@ -156,16 +171,16 @@ function desperation(states) {
 			'duration'	: 5 * states.length
 		}
 	});
-	
+
 	// PUT http request to set a cycle of states representing each part in the story
 	request(options, function(error, response, body) {
-		if(error) console.log(error) 
+		if(error) console.log(error)
 	}); // end of request
 
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 exports.breathe = breathe;
-exports.fadeOff = fadeOff;
+exports.turnOff = turnOff;
 exports.desperation = desperation;
 exports.validButtonPress = validButtonPress;
