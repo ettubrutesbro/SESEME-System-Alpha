c40aa30f4d848aebd6a005aa19d96a77f0b28641
tbbui <tbbui@ucdavis.edu>
2015-10-01 19:21:16 -0700

    test

--- src/jsLibrary/hue.js
+++ src/jsLibrary/hue.js
@@ -9,16 +9,9 @@ var displayBridges = function(bridge) {
     console.log("Hue Bridges Found: " + JSON.stringify(bridge));
 };
 
-// --------------------------
-// Using a promise
-hue.nupnpSearch().then(displayBridges).done();
-
-// --------------------------
-// Using a callback
-hue.nupnpSearch(function(err, result) {
-    if (err) throw err;
-    displayBridges(result);
-});
+var displayResultVar = function(result) {
+    console.log(JSON.stringify(result, null, 2));
+};
 
 var HueApi = hue.HueApi;
 var lightState = hue.lightState;
