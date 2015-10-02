6568a3f5fd01b65f99322b4b4a23a7197eb89b42
Andrew Kwon <askwon@ucdavis.edu>
2015-10-02 11:47:49 -0700

    Added a check for when the seedling is offline for sim button

--- src/jsLibrary/websockets.js
+++ src/jsLibrary/websockets.js
@@ -312,11 +312,14 @@ io.on('connection', function (socket) {
 
   // Front-end simulation of a button press
   socket.on('sim button', function(seedlingNum) {
-      if(!seedlings[seedlingNum].buttonPressed){
+      if(!seedlings[seedlingNum].buttonPressed && seedlings[seedlingNum].online){
           console.log("Sim button pressed")
           seedlings[seedlingNum].buttonPressed = true;
           bigRedButton(seedlings[seedlingNum]);
-      } else { console.log('Wrong'); }
+      } else {
+          console.log('Wrong');
+          seedling.buttonPressed = false;
+      }
   });
 
   socket.on('sim button2', function(seedlingNum) {
