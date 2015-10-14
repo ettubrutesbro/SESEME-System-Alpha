c494b485b2e6d27b6dfb2cd293a633bb58fe1a38
Andrew Kwon <askwon@ucdavis.edu>
2015-10-13 19:43:21 -0700

    Added a slackbot remote integration message function for in case of seedling/monument connection

--- src/jsLibrary/slackbot.js
+++ src/jsLibrary/slackbot.js
@@ -2,6 +2,36 @@
 var request = require('request');
 var moment = require('moment');
 
+function reportConnection(title) {
+    var timestamp = moment().format("h:mm a* on ddd, M/D");
+    var options = {
+        // URI to send a message to the #diagnostic channel
+        uri: 'https://hooks.slack.com/services/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',
+        method: 'POST',
+        body: JSON.stringify({
+            "channel"        : "#diagnostic",
+            "username"        : "claptron",
+            "attachments": [
+                {
+                    "title"        : "~ :hurtrealbad: "+title+" :hurtrealbad: ~",
+                    "title_link": "http://www.seseme.net",
+                    "fallback"    : "*["+timestamp+"] Claptron reporting in: "
+                                    + "_hella' WET!",
+                    "pretext"    : "*["+timestamp+"] Claptron reporting in: " 
+                                    + "_hella WET!",
+                    "color"        : "green",
+                    "mrkdwn_in": ["text", "pretext"]
+                }
+            ] // end of attachments
+        }) // end of body
+    };
+
+    // PUT http request to update the hue color
+    request(options, function(error, response, body) {
+        if(error) console.log("Claptron Error: " + error);
+    }); // end of request
+}
+
 function reportDisconnect(title) {
     var timestamp = moment().format("h:mm a* on ddd, M/D");
     var options = {
@@ -15,8 +45,10 @@ function reportDisconnect(title) {
                 {
                     "title"        : "~ :hurtrealbad: "+title+" :hurtrealbad: ~",
                     "title_link": "http://www.seseme.net",
-                    "fallback"    : "*["+timestamp+"] Claptron reporting in: _fuckin' garbage_!",
-                    "pretext"    : "*["+timestamp+"] Claptron reporting in: _fuckin' garbage_!",
+                    "fallback"    : "*["+timestamp+"] Claptron reporting in: "
+                                    + "_fuckin' garbage_!",
+                    "pretext"    : "*["+timestamp+"] Claptron reporting in: " 
+                                    + "_fuckin' garbage_!",
                     "color"        : "#f30020",
                     "mrkdwn_in": ["text", "pretext"]
                 }
@@ -47,8 +79,10 @@ function reportSysCheck(systemStatus, pretext) {
                 {
                     "title"        : "Periodic System Check",
                     "title_link": "http://www.seseme.net",
-                    "fallback"    : "*["+timestamp+"] Claptron reporting in: _"+pretext+"_!",
-                    "pretext"    : "*["+timestamp+"] Claptron reporting in: _"+pretext+"_!",
+                    "fallback"    : "*["+timestamp+"] Claptron reporting in: _"
+                                    + pretext+"_!",
+                    "pretext"    : "*["+timestamp+"] Claptron reporting in: _"
+                                    + pretext + "_!",
                     "color"        : "#47515b",
                     "mrkdwn_in": ["text", "pretext"],
                 },
@@ -116,3 +150,4 @@ function reportSysCheck(systemStatus, pretext) {
 
 exports.reportSysCheck = reportSysCheck;
 exports.reportDisconnect = reportDisconnect;
+exports.reportConnection = reportConnection;
