1b60ddf1d23e8b7904f1779e57ffa2ca10794a6c
Andrew Kwon <askwon@ucdavis.edu>
2015-10-17 22:21:33 -0700

    Implemented function for claptron to report the ping results

--- src/xps/slackbot.js
+++ src/xps/slackbot.js
@@ -9,18 +9,18 @@ function reportConnection(title) {
         uri: 'https://hooks.slack.com/services/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',
         method: 'POST',
         body: JSON.stringify({
-            "channel"        : "#diagnostic",
-            "username"        : "claptron",
-            "attachments": [
+            "channel"       : "#diagnostic",
+            "username"      : "claptron",
+            "attachments"   : [
                 {
-                    "title"        : ":hurtrealbad: "+title+" :hurtrealbad: ~",
-                    "title_link": "http://www.seseme.net",
-                    "fallback"    : "*["+timestamp+"] Claptron reporting in: "
-                                    + "_hella WET",
-                    "pretext"    : "*["+timestamp+"] Claptron reporting in: " 
-                                    + "_hella WET",
-                    "color"        : "green",
-                    "mrkdwn_in": ["text", "pretext"]
+                    "title"         : ":cubimal_chick: "+title+" :cubimal_chick:",
+                    "title_link"    : "http://www.seseme.net",
+                    "fallback"      : "*["+timestamp+"] Claptron reporting in: "
+                                      + "_hella WET",
+                    "pretext"       : "*["+timestamp+"] Claptron reporting in: " 
+                                      + "_hella WET",
+                    "color"         : "green",
+                    "mrkdwn_in"     : ["text", "pretext"]
                 }
             ] // end of attachments
         }) // end of body
@@ -39,17 +39,17 @@ function reportDisconnect(title) {
         uri: 'https://hooks.slack.com/services/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',
         method: 'POST',
         body: JSON.stringify({
-            "channel"        : "#diagnostic",
-            "username"        : "claptron",
-            "attachments": [
+            "channel"       : "#diagnostic",
+            "username"      : "claptron",
+            "attachments"   : [
                 {
-                    "title"        : ":hurtrealbad: "+title+" :hurtrealbad:",
-                    "title_link": "http://www.seseme.net",
-                    "fallback"    : "*["+timestamp+"] Claptron reporting in: "
-                                    + "_fuckin garbage_",
-                    "pretext"    : "*["+timestamp+"] Claptron reporting in: " 
-                                    + "_fuckin garbage_",
-                    "color"        : "#f30020",
+                    "title"         : ":hurtrealbad: "+title+" :hurtrealbad:",
+                    "title_link"    : "http://www.seseme.net",
+                    "fallback"      : "*["+timestamp+"] Claptron reporting in: "
+                                      + "_fuckin garbage_",
+                    "pretext"       : "*["+timestamp+"] Claptron reporting in: " 
+                                      + "_fuckin garbage_",
+                    "color"         : "#f30020",
                     "mrkdwn_in": ["text", "pretext"]
                 }
             ] // end of attachments
@@ -73,68 +73,68 @@ function reportSysCheck(systemStatus, pretext) {
         uri: 'https://hooks.slack.com/services/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',
         method: 'POST',
         body: JSON.stringify({
-            "channel"        : "#diagnostic",
-            "username"        : "claptron",
-            "attachments": [
+            "channel"       : "#diagnostic",
+            "username"      : "claptron",
+            "attachments"   : [
                 {
-                    "title"        : "Periodic System Check",
+                    "title"         : "Periodic System Check",
                     "title_link": "http://www.seseme.net",
-                    "fallback"    : "*["+timestamp+"] Claptron reporting in: _"
-                                    + pretext+"_!",
-                    "pretext"    : "*["+timestamp+"] Claptron reporting in: _"
-                                    + pretext + "_!",
-                    "color"        : "#47515b",
+                    "fallback"      : "*["+timestamp+"] Claptron reporting in: _"
+                                      + pretext+"_!",
+                    "pretext"       : "*["+timestamp+"] Claptron reporting in: _"
+                                      + pretext + "_!",
+                    "color"         : "#47515b",
                     "mrkdwn_in": ["text", "pretext"],
                 },
                 {
-                    "color"        : monumentColor,
-                    "fields"    : [
+                    "color"         : monumentColor,
+                    "fields"        : [
                         {
-                            "value":  "SESEME Monument",
-                            "short": true
+                            "value" :  "SESEME Monument",
+                            "short" : true
                         },
                         {
-                            "value":  "[" + systemStatus.monument +"]",
-                            "short": true
+                            "value" :  "[" + systemStatus.monument +"]",
+                            "short" : true
                         }
                     ] // end of fields
                 },
                 {
-                    "color"        : pi1Color,
-                    "fields"    : [
+                    "color"         : pi1Color,
+                    "fields"        : [
                         {
-                            "value": "Seedling 1",
-                            "short": true
+                            "value" : "Seedling 1",
+                            "short" : true
                         },
                         {
-                            "value":  "[" + systemStatus.pi1 + "]",
-                            "short": true
+                            "value" :  "[" + systemStatus.pi1 + "]",
+                            "short" : true
                         }
                     ] // end of fields
                 },
                 {
-                    "color"        : pi2Color,
-                    "fields"    : [
+                    "color"         : pi2Color,
+                    "fields"        : [
                         {
-                            "value": "Seedling 2",
-                            "short": true
+                            "value" : "Seedling 2",
+                            "short" : true
                         },
                         {
-                            "value":  "[" + systemStatus.pi2 + "]",
-                            "short": true
+                            "value" :  "[" + systemStatus.pi2 + "]",
+                            "short" : true
                         }
                     ] // end of fields
                 },
                 {
-                    "color"        : pi3Color,
-                    "fields"    : [
+                    "color"         : pi3Color,
+                    "fields"        : [
                         {
-                            "value": "Seedling 3",
-                            "short": true
+                            "value" : "Seedling 3",
+                            "short" : true
                         },
                         {
-                            "value":  "[" + systemStatus.pi3 + "]",
-                            "short": true
+                            "value" :  "[" + systemStatus.pi3 + "]",
+                            "short" : true
                         }
                     ] // end of fields
                 }
@@ -148,6 +148,40 @@ function reportSysCheck(systemStatus, pretext) {
     }); // end of request
 }
 
-exports.reportSysCheck = reportSysCheck;
-exports.reportDisconnect = reportDisconnect;
-exports.reportConnection = reportConnection;
+function reportPing(title, error) {
+    var title;
+    var message = error || "hella WET";
+    var color = error ? "red" : "green";
+    var timestamp = moment().format("h:mm a* on ddd, M/D");
+    var options = {
+        // URI to send a message to the #diagnostic channel
+        uri: 'https://hooks.slack.com/services/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',
+        method: 'POST',
+        body: JSON.stringify({
+            "channel"       : "#diagnostic",
+            "username"      : "claptron",
+            "attachments"   : [
+                {
+                    "title"         : title, 
+                    "title_link"    : "http://www.seseme.net",
+                    "fallback"      : "*["+timestamp+"] Claptron reporting in: "
+                                      + message,
+                    "pretext"       : "*["+timestamp+"] Claptron reporting in: " 
+                                      + message,
+                    "color"         : color,
+                    "mrkdwn_in": ["text", "pretext"]
+                }
+            ] // end of attachments
+        }) // end of body
+    };
+
+    // PUT http request to update the hue color
+    request(options, function(error, response, body) {
+        if(error) console.log("Claptron Error: " + error);
+    }); // end of request
+}
+
+exports.reportPing          = reportPing;
+exports.reportSysCheck      = reportSysCheck;
+exports.reportDisconnect    = reportDisconnect;
+exports.reportConnection    = reportConnection;
