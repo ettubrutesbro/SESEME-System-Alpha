f1c1e2098a1fda974a940d4850ec185d3e7a9686
Andrew Kwon <askwon@ucdavis.edu>
2015-11-05 13:55:08 -0800

    Changed the print module to return the console.log function

--- src/jsLibrary/print.js
+++ src/jsLibrary/print.js
@@ -1,5 +1,5 @@
 function print(message) {
-    console.log(moment().format('〈MM/DD | h:mm:ss a 〉'));
+	return console.log(moment().format('〈MM/DD | h:mm:ss a 〉') + message);
 }
 
 exports.print = print;
