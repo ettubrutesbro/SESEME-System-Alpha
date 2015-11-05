0d442c76b4b6bf53849e17ed3c34bb4d3a829ce0
Andrew Kwon <askwon@ucdavis.edu>
2015-11-05 14:00:00 -0800

    Added an explicit function enclosure around console log

--- src/jsLibrary/print.js
+++ src/jsLibrary/print.js
@@ -1,6 +1,8 @@
 var moment = require('moment');
 function print(message) {
-	return console.log(moment().format('〈MM/DD | h:mm:ss a 〉') + message);
+	return function(message) { 
+        console.log(moment().format('〈MM/DD | h:mm:ss a 〉') + message) 
+    };
 }
 
 exports.print = print;
