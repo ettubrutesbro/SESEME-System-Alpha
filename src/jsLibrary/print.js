85eb982e39da45489e3e64344b4e1f7b5818671f
Andrew Kwon <askwon@ucdavis.edu>
2015-11-08 02:19:26 -0800

    Updated logging styles

--- src/jsLibrary/print.js
+++ src/jsLibrary/print.js
@@ -1,6 +1,8 @@
 var moment = require('moment');
+var colors = require('colors');
 var print = function(message) {
-    console.log(moment().format('〈MM/DD | h:mm:ss a 〉') + message) 
+    console.log('['.bold + moment().format('MM/DD').bold.gray+ '|' 
+                + moment().format('h:mm:ss a').gray + '] '.bold + message);
 }
 
 module.exports = print;
