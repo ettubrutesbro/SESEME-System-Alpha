f191543a074cde344e81b3c9d1a693eaa2b4419c
Andrew Kwon <askwon@ucdavis.edu>
2016-04-18 15:19:10 -0700

    Updated stories for database insertion

--- src/xps/init/init.js
+++ src/xps/init/init.js
@@ -1,10 +1,14 @@
+// Format json using http://jsonviewer.stack.hu
+// Validate using http://jsonlint.com
+
 'use strict'
 var mongo = require('../stories.js');
 
 mongo.connect().then(db => {
-    db.collection('environment').save(require('./annual_energy_costs.json'));
     db.collection('environment').save(require('./cool_schools.json'));
+    db.collection('environment').save(require('./ucd_building_annual_energy_costs.json'));
+    db.collection('environment').save(require('./energy_use_intensity.json'));
     db.collection('society').save(require('./wage_inequity.json'));
     db.collection('society').save(require('./mass_incarceration.json'));
-    db.collection('misc').save(require('./college_stats.json'));
+    db.collection('misc').save(require('./cost_of_education.json'));
 });
