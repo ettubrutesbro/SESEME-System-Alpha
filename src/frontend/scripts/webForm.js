d0ba21ee6836b217b19fd32c6b876db094c0d84e
ettubrutesbro <Jack.Leng@gmail.com>
2016-06-03 03:00:16 -0700

    establishing some ideas

--- src/frontend/scripts/webForm.js
+++ src/frontend/scripts/webForm.js
@@ -1,6 +1,23 @@
-/**
- * Created by coreyching on 1/29/16.
- */
-function loadWebPage() {
-    console.log('hi');
+
+
+function generateForm(){
+	// document.getElementById('formeditor')
+	for(var i = 0; i<4; i++){ //recursively create fields for pillars here
+		//create variables that are createElements for inputs
+		//pillar name (pNames[i])
+		//pillar value (values[i])
+		//pillar text (pText[i])
+
+
+	}
+}
+
+function createForm(){
+
+	//v1 implementation - DOM structure sits in normal app with no content and display disabled, until this function
+	//populates it with information and inputs for admins to change / add story content
+
+
+
+
 }
